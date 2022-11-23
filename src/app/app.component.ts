c809c471aee3818cd28ce193e3dc9c3ee82474b7
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-11-23 11:50:53 +0900

    remove app module

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,13 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
+  imports: [CommonModule, RouterModule],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  standalone: true,
 })
 export class AppComponent {
   title = 'photo-gallery-app';
