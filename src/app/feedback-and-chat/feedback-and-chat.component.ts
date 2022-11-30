306b75f5683754a1e583d446565543f9de674631
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-11-30 22:56:11 +0900

    implements feedback-and-chat component

--- src/app/feedback-and-chat/feedback-and-chat.component.ts
+++ src/app/feedback-and-chat/feedback-and-chat.component.ts
@@ -1,15 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-feedback-and-chat',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './feedback-and-chat.component.html',
   styleUrls: ['./feedback-and-chat.component.css'],
 })
 export class FeedbackAndChatComponent implements OnInit {
-  constructor() {}
+  showMsg: boolean = false;
+  private formBuilder = inject(FormBuilder);
+
+  contactForm = this.formBuilder.group({
+    fullname: '',
+    email: '',
+    comments: '',
+  });
 
   ngOnInit(): void {}
+
+  onSubmit(): void {
+    console.log('Your feedback has been submitted', this.contactForm.value);
+    this.showMsg = true;
+    this.contactForm.reset();
+  }
 }
