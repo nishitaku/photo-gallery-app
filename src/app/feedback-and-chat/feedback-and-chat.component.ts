fb62be35399d6b0bb48ff3d074195b7e4d41dff7
nishitaku <29690066+nishitaku@users.noreply.github.com>
2022-11-30 22:40:21 +0900

    ng generate feedback-and-chat component --standalone

---
+++ src/app/feedback-and-chat/feedback-and-chat.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-feedback-and-chat',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './feedback-and-chat.component.html',
+  styleUrls: ['./feedback-and-chat.component.css'],
+})
+export class FeedbackAndChatComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
